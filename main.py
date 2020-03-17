7d2395431bdffd538ee78f8ea4b3cd6652613851
Alex Boiko <alexboyko94@gmail.com>
2020-03-17 13:10:52 +0100

    dd

--- main.py
+++ main.py
@@ -34,9 +34,11 @@ app = Flask(__name__)
 import urllib.request
 # Model saved with Keras model.save()
 MODEL_PATH = 'https://drive.google.com/uc?export=download&id=1LKA8sV9Wz8yRc88TRsmkAcpG0y5Sojp_'
-urllib.request.urlretrieve(MODEL_PATH, 'model.h5')
+print('Downloading the model...')
+urllib.request.urlretrieve(MODEL_PATH, 'models/model.h5')
+print('Model is downloaded.')
 # Load your own trained model
-model = load_model('model.h5')
+model = load_model('models/model.h5')
 model._make_predict_function()          # Necessary
 print('Model loaded. Start serving...Check http://127.0.0.1:5000/')
 
@@ -99,7 +101,7 @@ def predict():
     return None
 
 if __name__ == '__main__':
-    app.run(port=5002, threaded=False)
+    app.run(port=5000, threaded=False)
     # Serve the app with gevent
     http_server = WSGIServer(('0.0.0.0', 5000), app)
     http_server.serve_forever()

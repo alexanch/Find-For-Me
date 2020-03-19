325633c6289cae208aaa3558b606dc2984d9dd73
Alex Boiko <alexboyko94@gmail.com>
2020-03-19 13:17:21 +0100

    web version: 1.0
    Updates:
    - proper animation
    - updated logics for button events

--- static/main.js
+++ static/main.js
@@ -33,31 +33,43 @@ function fileSelectHandler(e) {
 //========================================================================
 
 var imagePreview = document.getElementById("image-preview");
-var imageDisplay = document.getElementById("image-display");
 var uploadCaption = document.getElementById("upload-caption");
 var predResult = document.getElementById("pred-result");
 var loader = document.getElementById("loader");
 var buttonCloth = document.getElementById("button-cloth")
+var buttonSubmit = document.getElementById('submit-button')
 var panel = document.getElementById('panel')
 
 //========================================================================
 // Main button events
 //========================================================================
 
+function previewFile(file) {
+// show the preview of the image
+  console.log(file.name);
+  var fileName = encodeURI(file.name);
+  var reader = new FileReader();
+  reader.readAsDataURL(file);
+  reader.onloadend = () => {
+  imagePreview.src = reader.result;
+  show(imagePreview);
+  hide(uploadCaption);
+
+  }
+}
+
 function submitImage() {
+
   // action for the submit button
   console.log("submit");
-
-  if (!imageDisplay.src || !imageDisplay.src.startsWith("data")) {
+  var imgPreview = document.getElementById('image-preview');
+  if (!imgPreview.src || !imgPreview.src.startsWith("data")) {
     window.alert("Please select an image before submit.");
     return;
   }
-
   loader.classList.remove("hidden");
-  imageDisplay.classList.add("loading");
-
   // call the predict function of the backend
-  predictImage(imageDisplay.src);
+  predictImage(imgPreview.src);
 }
 
 function clearImage() {
@@ -65,39 +77,16 @@ function clearImage() {
   fileSelect.value = "";
   // remove image sources and hide them
   imagePreview.src = "";
-  imageDisplay.src = "";
-  predResult.innerHTML = "";
-  hide(document.getElementById('pred-result2'))
-  hide(imagePreview);
-  hide(imageDisplay);
   hide(loader);
-  hide(buttonCloth);
-  hide(predResult);
-  show(uploadCaption);
-  imageDisplay.classList.remove("loading");
+  hide(imagePreview);
+  show(buttonSubmit);
+  show(panel);
+  //
   document.getElementById('pred-result2').innerHTML = "";
+  hide(document.getElementById('pred-result2'))
 
-}
-
-function previewFile(file) {
-  // show the preview of the image
-  console.log(file.name);
-  var fileName = encodeURI(file.name);
-
-  var reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onloadend = () => {
-    imagePreview.src = URL.createObjectURL(file);
-
-    show(imagePreview);
-    hide(uploadCaption);
-
-    // reset
-    predResult.innerHTML = "";
-    imageDisplay.classList.remove("loading");
-
-    displayImage(reader.result, "image-display");
-  };
+  hide(buttonCloth);
+  show(uploadCaption);
 }
 
 //========================================================================
@@ -115,7 +104,7 @@ function predictImage(image) {
     .then(resp => {
       if (resp.ok)
         resp.json().then(data => {
-          displayResult(data);
+          displayGrid(data);
         });
     })
     .catch(err => {
@@ -124,40 +113,14 @@ function predictImage(image) {
     });
 }
 
-function displayImage(image, id) {
-  // display image on given id <img> element
-  let display = document.getElementById(id);
-  display.src = image;
-  show(display);
-}
+function displayGrid(data){
+   // display the result
+   //imageDisplay.classList.remove("loading");
+   //predResult.innerHTML = data.result;
+    hide(panel);
+    hide(buttonSubmit);
+    show(buttonCloth);
 
-function displayResult(data) {
-  // display the result
-  // imageDisplay.classList.remove("loading");
-  hide(loader);
-  //predResult.innerHTML = data.result;
-  hide(imageDisplay);
-  hide(panel);
-  show(buttonCloth);
-  //displayImage('https://media-exp1.licdn.com/dms/image/C4D03AQFiMJ8MbeNmrQ/profile-displayphoto-shrink_200_200/0?e=1590019200&v=beta&t=XjlSBOZZGmwMJGjQd3EcsVbA3wkLfQCezlDFS3zlj5A','pred-result');
-  display_grid(data);
-}
-
-function hide(el) {
-  // hide an element
-  el.classList.add("hidden");
-}
-
-function show(el) {
-  // show an element
-  el.classList.remove("hidden");
-}
-
-//function show_res(data) {
-//    image = data['images.model']
-//    displayImage(image,text_to_show)
-//}
-function display_grid(data){
     var i, obj = JSON.parse(data);
     var text = '';
     for (i in obj) {
@@ -178,3 +141,43 @@ function display_grid(data){
         document.getElementById('pred-result2').appendChild(newDiv);
     }
 }
+
+function hide(el) {
+  // hide an element
+  el.classList.add("hidden");
+}
+
+function show(el) {
+  // show an element
+  el.classList.remove("hidden");
+}
+
+
+//  // show the preview of the image
+//  console.log(file.name);
+//  var fileName = encodeURI(file.name);
+//
+//  var reader = new FileReader();
+//  reader.readAsDataURL(file);
+//  reader.onloadend = () => {
+//    imagePreview.src = URL.createObjectURL(file);
+//
+//    show(imagePreview);
+//    hide(uploadCaption);
+//
+//    // reset
+//    predResult.innerHTML = "";
+//    imageDisplay.classList.remove("loading");
+//    let display = document.getElementById("image-display");
+//    display.src = reader.result;
+//    show(display);
+//  };
+//}
+
+
+//function displayImage(image, id) {
+//  // display image on given id <img> element
+//  let display = document.getElementById(id);
+//  display.src = image;
+//  show(display);
+//}
